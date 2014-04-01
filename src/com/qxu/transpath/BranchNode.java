16352e7b9159bc4f16beb7b54bf70139e16e015d
Skylynx13 <skylynx13@gmail.com>
2014-04-02 00:15:34 +0800

    BranchNode checked in.

---
+++ src/com/qxu/transpath/BranchNode.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2014,qxu. 
+ * All Rights Reserved.
+ * 
+ * Project Name:transpath
+ * Package Name:com.qxu.transpath
+ * File Name:BranchNode.java
+ * Date:2014-4-2 上午12:14:48
+ * 
+ */
+package com.qxu.transpath;
+
+ /**
+ * ClassName: BranchNode <br/>
+ * Description: TODO <br/>
+ * Date: 2014-4-2 上午12:14:48 <br/>
+ * <br/>
+ * 
+ * @author qxu@
+ * 
+ * Change Log:
+ * @version yyyy-mm-dd qxu@<br/>
+ * 
+ */
+
+public class BranchNode {
+
+}
