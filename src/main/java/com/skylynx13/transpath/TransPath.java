01741e00001089c2d616ebca8d4c0c4422827eb0
skylynx13 <skylynx13@sina.com>
2018-12-04 22:31:09 +0800

    Start a new TransPath entry.

---
+++ src/main/java/com/skylynx13/transpath/TransPath.java
@@ -0,0 +1,10 @@
+package com.skylynx13.transpath;
+
+import com.skylynx13.transpath.ui.TranspathFrame;
+
+public class TransPath {
+    public static void main(String[] args) {
+        System.out.println("any problem?");
+        new TranspathFrame();
+    }
+}
