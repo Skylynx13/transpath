2a9544af2d2c18d2db9a9fa4cfd4f3136aa76762
Skylynx <skylynx13@sina.com>
2019-02-21 17:13:56 +0800

    Remove unused dependencies; Removed compress utils.

--- src/test/java/com/skylynx13/transpath/utils/FileUtilsTest.java
+++ src/test/java/com/skylynx13/transpath/utils/FileUtilsTest.java
@@ -131,30 +131,4 @@ public class FileUtilsTest {
 
         assertEquals(f1.length() + f2.length(), f3.length());
     }
-
-    /**
-     * To pass this test, add following entries to ${JRE_HOME}\lib\content-types.properties
-     * text/qxu: \
-     *     description=QXU text;\
-     *     file_extensions=.qxu
-     *
-     * archive/roshal: \
-     *     description=rar package;\
-     *     file_extensions=.rar
-     *
-     * archive/comic: \
-     *     description=comic package;\
-     *     file_extensions=.cbr,.cbz
-     */
-    @Test
-    public void getFileMimeTypeTest() {
-        long tm1 = System.currentTimeMillis();
-        assertEquals("text/plain", FileUtils.getFileMimeType(testResource("getFileMimeType_000.txt")));
-        System.out.println("getFileMimeTypeTest URLConnection: " + (
-                System.currentTimeMillis()-tm1) + " ms.");
-//        assertEquals("text/qxu", FileUtils.getFileMimeType(testResource("getFileMimeType_001.qxu")));
-//        assertEquals("archive/roshal", FileUtils.getFileMimeType(testResource("getFileMimeType_002.rar")));
-//        assertEquals("archive/comic", FileUtils.getFileMimeType(testResource("getFileMimeType_003.cbr")));
-//        assertEquals("archive/comic", FileUtils.getFileMimeType(testResource("getFileMimeType_004.cbz")));
-    }
 }
